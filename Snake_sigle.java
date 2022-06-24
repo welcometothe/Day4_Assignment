d799a419b782b9a2267ff1c829c860f87e814a26
unknown <vaibhavpatil100008@gmail.com>
2022-06-24 13:47:30 +0530

    this is my third commit

---
+++ Snake_sigle.java
@@ -0,0 +1,8 @@
+public class snake_single {
+    public static void main(String[] darsh) {
+        int PlayerPosition = 0;
+        System.out.println("Welcome to Snake and Ladder Simulation");
+        System.out.println("Player Position: " + PlayerPosition);
+
+    }
+}
